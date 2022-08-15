bf2bad671f96b34467a11608e81cdb50586ce4ef
SRoy1997 <sarah@devpipeline.com>
2022-08-14 19:47:37 -0600

    edited auth pages

--- src/components/LoginAuth/helpers/UserRoles.js
+++
@@ -1,9 +0,0 @@
-import WithAuth from "../core/WithAuth";
-
-export const StandardUser = WithAuth([
-  "user",
-  "admin",
-  "super-admin",
-  "standard",
-]);
-export const AdminUser = WithAuth(["admin", "super-admin"]);
